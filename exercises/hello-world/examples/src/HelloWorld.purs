827e669aa24dc3d6db1f483d3a2dc668ee91109e
Louis Pilfold <louis@lpil.uk>
2016-12-05 00:17:31 +0000

    Add hello-world exercise

---
+++ exercises/hello-world/examples/src/HelloWorld.purs
@@ -0,0 +1,8 @@
+module Main where
+
+import Prelude
+import Data.Maybe (Maybe(Just, Nothing))
+
+helloWorld :: Maybe String -> String
+helloWorld Nothing = "Hello, World!"
+helloWorld (Just x) = "Hello, " <> x <> "!"
