c8142f918214ceaf01937d5550ae0dd4d0606d38
icyrockcom <icyrock.com@icyrock.com>
2017-03-20 21:57:38 -0400

    Added support for Travis cache (issue #23) (#24)

--- bin/test.sh
+++ bin/test.sh
@@ -2,21 +2,45 @@
 
 # TEST
 
+xpurescript=$(dirname "$BASH_SOURCE")
+xpurescript=$(readlink -f "$xpurescript/..")
+cd "$xpurescript"
+echo "xpurescript: $xpurescript"
+
+cache_dir="$HOME"
+if [ -z "$TRAVIS" ]; then
+  cache_dir="$xpurescript"
+fi
+cache_dir="$cache_dir/.exercise_cache"
+echo "cache_dir: $cache_dir"
+
 declare -i TEST_RESULT=0
 FAILED_EXERCISES=''
 
-for exercise_dir in exercises/**
+cd exercises
+
+for exercise_dir in *
 do
-  exercise_src="$exercise_dir/src"
-  exercise_examples_src="$exercise_dir/examples/src"
-
-  mv "$exercise_src" "$exercise_src.impl"
-  mv "$exercise_examples_src" "$exercise_src"
-
   echo "-------------------------------------------------------"
   echo "Testing $exercise_dir"
 
   cd "$exercise_dir"
+
+  exercise_src=src
+  exercise_examples_src=examples/src
+
+  # Setup Travis cache
+  for dir in bower_components output; do
+    cache="$cache_dir/$exercise_dir/$dir"
+
+    mkdir -p "$cache"
+    ln -f -s "$cache"
+    echo "cache: $(ls -l "$dir")"
+  done
+
+  mv "$exercise_src" "$exercise_src.impl"
+  mv "$exercise_examples_src" "$exercise_src"
+
   bower install
   pulp test
 
@@ -27,9 +51,10 @@ do
   fi
 
   # be kind, rewind
-  cd -
   mv "$exercise_src" "$exercise_examples_src"
   mv "$exercise_src.impl" "$exercise_src"
+
+  cd -
 done
 
 if [ $TEST_RESULT -ne 0 ]; then
