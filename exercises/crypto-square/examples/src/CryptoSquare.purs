b1b423227c51b3e9c9739748549c6959ec70ac0a
icyrockcom <icyrock.com@icyrock.com>
2017-04-17 13:40:30 -0400

    Added Crypto square exercise (#41)

---
+++ exercises/crypto-square/examples/src/CryptoSquare.purs
@@ -0,0 +1,63 @@
+module CryptoSquare
+  ( normalizedPlaintext
+  , plaintextSegments
+  , encoded
+  , ciphertext
+  ) where
+
+import Prelude
+import Data.Array as A
+import Data.Array (filter, fromFoldable, replicate, toUnfoldable, (:))
+import Data.Char.Unicode (isAlphaNum)
+import Data.Foldable (maximum)
+import Data.Int (ceil, toNumber)
+import Data.List (transpose)
+import Data.Maybe (fromMaybe)
+import Data.String (drop, fromCharArray, joinWith, length, take, toCharArray, toLower)
+import Math (sqrt)
+
+normalizedPlaintext :: String -> String
+normalizedPlaintext
+  = toCharArray
+    >>> filter isAlphaNum
+    >>> fromCharArray
+    >>> toLower
+
+plaintextSegments :: String -> Array String
+plaintextSegments str = toSquare norm
+  where norm = normalizedPlaintext str
+        cols = sqrt (length norm # toNumber) # ceil
+        toSquare "" = []
+        toSquare s  = take cols s : toSquare (drop cols s)
+
+transposeArray :: forall a. Array (Array a) -> Array (Array a)
+transposeArray
+  = toUnfoldable
+    >>> map toUnfoldable
+    >>> transpose
+    >>> map fromFoldable
+    >>> fromFoldable
+
+encoded :: String -> String
+encoded = plaintextSegments
+          >>> map toCharArray
+          >>> transposeArray
+          >>> map fromCharArray
+          >>> joinWith ""
+
+spaces :: Int -> Array Char
+spaces n = replicate n ' '
+
+equalPad :: Array (Array Char) -> Array (Array Char)
+equalPad arr = map pad arr
+  where width = fromMaybe 0 (maximum $ map A.length arr)
+        pad el = el <> spaces (width - A.length el)
+
+ciphertext :: String -> String
+ciphertext = plaintextSegments
+             >>> map toCharArray
+             >>> transposeArray
+             >>> equalPad
+             >>> map fromCharArray
+             >>> joinWith " "
+
---
+++ exercises/crypto-square/test/Main.purs
@@ -0,0 +1,92 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Test.Unit.Assert as Assert
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+import CryptoSquare ( normalizedPlaintext
+                    , plaintextSegments
+                    , encoded
+                    , ciphertext
+                    )
+
+main :: Eff _ Unit
+main = runTest do
+
+  suite "CryptoSquare.normalizedPlaintext" do
+
+    test "Lowercase" $
+      Assert.equal "hello"
+                   (normalizedPlaintext "Hello")
+
+    test "Remove spaces" $
+      Assert.equal "hithere"
+                   (normalizedPlaintext "Hi there")
+
+    test "Remove punctuation" $
+      Assert.equal "123go"
+                   (normalizedPlaintext "@1, 2%, 3 Go!")
+
+  suite "CryptoSquare.plaintextSegments" do
+
+    test "empty plaintext results in an empty rectangle" $
+      Assert.equal
+        []
+        (plaintextSegments "")
+
+    test "4 character plaintext results in an 2x2 rectangle" $
+      Assert.equal
+        [ "ab"
+        , "cd"
+        ]
+        (plaintextSegments "Ab Cd")
+
+    test "9 character plaintext results in an 3x3 rectangle" $
+      Assert.equal
+        [ "thi"
+        , "sis"
+        , "fun"
+        ]
+        (plaintextSegments "This is fun!")
+
+    test "54 character plaintext results in an 8x7 rectangle" $
+      Assert.equal
+        [ "ifmanwas"
+        , "meanttos"
+        , "tayonthe"
+        , "groundgo"
+        , "dwouldha"
+        , "vegivenu"
+        , "sroots"
+        ]
+        (plaintextSegments "If man was meant to stay on the ground, god would have given us roots.")
+
+  suite "CryptoSquare.encoded" do
+
+    test "empty plaintext results in an empty encode" $
+      Assert.equal
+        ""
+        (encoded "")
+
+    test "Non-empty plaintext results in the combined plaintext segments" $
+      Assert.equal
+        "imtgdvsfearwermayoogoanouuiontnnlvtwttddesaohghnsseoau"
+        (encoded "If man was meant to stay on the ground, god would have given us roots.")
+
+  suite "CryptoSquare.ciphertext" do
+
+    test "empty plaintext results in an empty ciphertext" $
+      Assert.equal
+        ""
+        (ciphertext "")
+
+    test "9 character plaintext results in 3 chunks of 3 characters" $
+      Assert.equal
+        "tsf hiu isn"
+        (ciphertext "This is fun!")
+
+    test "54 character plaintext results in 7 chunks, the last two padded with spaces" $
+      Assert.equal
+        "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
+        (ciphertext "If man was meant to stay on the ground, god would have given us roots.")
