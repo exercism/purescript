56495a93d44fb80d7ec245905b7a21a59dce8253
icyrockcom <icyrock.com@icyrock.com>
2017-04-25 16:39:32 -0400

    Added Largest Series Product exercise (#44)

---
+++ exercises/largest-series-product/examples/src/LargestSeriesProduct.purs
@@ -0,0 +1,31 @@
+module LargestSeriesProduct
+  ( largestProduct
+  ) where
+
+import Prelude
+import Data.Array (drop, length, take, (:))
+import Data.Foldable (maximum, product)
+import Data.Int (fromString)
+import Data.Maybe (Maybe(..))
+import Data.String (singleton, toCharArray)
+import Data.Traversable (sequence)
+
+spans :: forall a. Int -> Array a -> Array (Array a)
+spans n a
+  | length a < n = []
+  | otherwise    = take n a : spans n (drop 1 a)
+
+toIntArray :: String -> Maybe (Array Int)
+toIntArray = toCharArray
+             >>> map (singleton >>> fromString) 
+             >>> sequence
+
+largestProduct :: String -> Int -> Maybe Int
+largestProduct digits span
+  | span == 0 = Just 1
+  | span < 0  = Nothing
+  | otherwise = do
+    is <- toIntArray digits
+    spans span is
+      # map product
+      # maximum
