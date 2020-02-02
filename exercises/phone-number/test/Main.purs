26f1922107c41af1bdba4b0aee60ae3e45de6961
Adam Morris <amorris2003@gmail.com>
2020-02-03 06:49:42 +1000

    Make the phone-number exercise use maybe (#123)
    * Make the phone-number exercise use maybe
    
    * remove unneeded brackets
    
    * hopefully remove errors
    
    * unchange bower

--- exercises/phone-number/test/Main.purs
+++ exercises/phone-number/test/Main.purs
@@ -7,6 +7,7 @@ import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Main (runTest)
 import Test.Unit.Assert as Assert
 import PhoneNumber (phoneNumber)
+import Data.Maybe (Maybe (..))
 
 main :: Effect Unit
 main = runTest suites
@@ -15,28 +16,26 @@ suites :: TestSuite
 suites = do
   suite "PhoneNumber.phoneNumber" do
     test "cleans the number" do
-      Assert.equal "2234567890" $ phoneNumber "(223) 456-7890"
+      Assert.equal (Just "2234567890") $ phoneNumber "(223) 456-7890"
     test "cleans numbers with dots" do
-      Assert.equal "2234567890" $ phoneNumber "223.456.7890"
+      Assert.equal (Just "2234567890") $ phoneNumber "223.456.7890"
     test "cleans numbers with multiple spaces" do
-      Assert.equal "2234567890" $ phoneNumber "223 456   7890   "
+      Assert.equal (Just "2234567890") $ phoneNumber "223 456   7890   "
     test "invalid when 9 digits" do
-      Assert.equal "None" $ phoneNumber "123456789"
+      Assert.equal Nothing $ phoneNumber "123456789"
     test "invalid when 11 digits does not start with a 1" do
-      Assert.equal "None" $ phoneNumber "22234567890"
+      Assert.equal Nothing $ phoneNumber "22234567890"
     test "valid when 11 digits and starting with 1" do
-      Assert.equal "2234567890" $ phoneNumber "12234567890"
+      Assert.equal (Just "2234567890") $ phoneNumber "12234567890"
     test "valid when 11 digits and starting with 1 even with punctuation" do
-      Assert.equal "2234567890" $ phoneNumber "+1 (223) 456-7890"
+      Assert.equal (Just "2234567890") $ phoneNumber "+1 (223) 456-7890"
     test "invalid when more than 11 digits" do
-      Assert.equal "None" $ phoneNumber "321234567890"
+      Assert.equal Nothing $ phoneNumber "321234567890"
     test "invalid with letters" do
-      Assert.equal "None" $ phoneNumber "123-abc-7890"
+      Assert.equal Nothing $ phoneNumber "123-abc-7890"
     test "invalid with punctuations" do
-      Assert.equal "None" $ phoneNumber "123-@:!-7890"
+      Assert.equal Nothing $ phoneNumber "123-@:!-7890"
     test "invalid if area code does not start with 2-9" do
-      Assert.equal "None" $ phoneNumber "(123) 456-7890"
+      Assert.equal Nothing $ phoneNumber "(123) 456-7890"
     test "invalid if exchange code does not start with 2-9" do
-      Assert.equal "None" $ phoneNumber "(223) 056-7890"
-
-
+      Assert.equal Nothing $ phoneNumber "(223) 056-7890"
