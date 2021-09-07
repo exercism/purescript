d95d9b2c6a2c9208188b17fa7c192ec5262f4b8d
icyrockcom <icyrock.com@icyrock.com>
2021-09-07 16:54:39 -0400

    Update pangram test to remove non-ASCII characters, closes #53

--- exercises/practice/pangram/test/Main.purs
+++ exercises/practice/pangram/test/Main.purs
@@ -14,27 +14,31 @@ main = runTest suites
 suites :: TestSuite
 suites = do
   suite "Pangram.isPangram" do
-    test "sentence empty" do
+    test "empty sentence" do
       Assert.equal false $
         isPangram ""
 
-    test "pangram with only lower case" do
+    test "perfect lower case" do
+      Assert.equal true $
+        isPangram "abcdefghijklmnopqrstuvwxyz"
+
+    test "only lower case" do
       Assert.equal true $
         isPangram "the quick brown fox jumps over the lazy dog"
 
-    test "missing character 'x'" do
+    test "missing the letter 'x'" do
       Assert.equal false $
         isPangram "a quick movement of the enemy will jeopardize five gunboats"
 
-    test "another missing character 'x'" do
+    test "missing the letter 'h'" do
       Assert.equal false $
-        isPangram "the quick brown fish jumps over the lazy dog"
+        isPangram "five boxing wizards jump quickly at it"
 
-    test "pangram with underscores" do
+    test "with underscores" do
       Assert.equal true $
         isPangram "the_quick_brown_fox_jumps_over_the_lazy_dog"
 
-    test "pangram with numbers" do
+    test "with numbers" do
       Assert.equal true $
         isPangram "the 1 quick brown fox jumps over the 2 lazy dogs"
 
@@ -42,10 +46,11 @@ suites = do
       Assert.equal false $
         isPangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"
 
-    test "pangram with mixed case and punctuation" do
+    test "mixed case and punctuation" do
       Assert.equal true $
         isPangram "\"Five quacking Zephyrs jolt my wax bed.\""
 
-    test "pangram with non ascii characters" do
-      Assert.equal true $
-        isPangram "Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich."
+    test "case insensitive" do
+      Assert.equal false $
+        isPangram "the quick brown fox jumps over with lazy FX"
+
