e288b2cbe51a69a4ed7b2ca99cf1b8fddc2d420b
Emiel van de Laar <gemiel@gmail.com>
2021-10-27 10:40:11 +0200

    Align forceful questions test with problem spec (#219)
    An additional response was added to the problem specification but the
    forceful question unit test was incorrect.
    
    The response should be "Calm down, I know what I'm doing!" because "WHAT
    THE HELL WERE YOU THINKING?" is both a question and a yell.
    
    See: exercism/purescript/issues/89 (awaiting to be reopened)

--- exercises/practice/bob/examples/src/Bob.purs
+++ exercises/practice/bob/examples/src/Bob.purs
@@ -11,35 +11,30 @@ import Partial.Unsafe as Partial
 
 hey :: String -> String
 hey msg =
-  if hasLetters msg && allUppercase msg then
-    "Whoa, chill out!"
+  if isYell msg && isQuestion msg then
+    "Calm down, I know what I'm doing!"
   else if isQuestion msg then
     "Sure."
+  else if isYell msg then
+    "Whoa, chill out!"
   else if isSilence msg then
     "Fine. Be that way!"
   else
     "Whatever."
 
-
-allUppercase :: String -> Boolean
-allUppercase str =
-  String.toUpper str == str
-
-
 isQuestion :: String -> Boolean
 isQuestion =
   testRegex "\\?$"
 
-
-hasLetters :: String -> Boolean
-hasLetters =
-  testRegex "[a-zA-Z]"
-
+isYell :: String -> Boolean
+isYell str = hasLetter str && allUpper str
+  where
+  hasLetter = testRegex "[a-zA-Z]"
+  allUpper s = String.toUpper s == s
 
 isSilence :: String -> Boolean
 isSilence =
   testRegex "^[\\s\\t\\n]*$"
-
 
 testRegex :: String -> String -> Boolean
 testRegex patternStr =
