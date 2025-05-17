a1854925ba0205ceaca36253b51e5957d57ba776
RainbowEncoder <145037490+RainbowEncoder@users.noreply.github.com>
2025-05-17 22:25:53 +0100

    correct spiral matrix test inputs (#316)
    [no important files changed]

--- exercises/practice/spiral-matrix/test/Main.purs
+++ exercises/practice/spiral-matrix/test/Main.purs
@@ -16,36 +16,36 @@ suites :: TestSuite
 suites =
   suite "Spiral.spiral" do
     test "empty spiral" $
-      (spiral 0) `equal` (Nil :: List (List Int))
+      equal (Nil :: List (List Int)) $ spiral 0
 
     test "trivial spiral" $
-      (spiral 1) `equal` (to2DList [[1]])
+      equal (to2DList [[1]]) $ spiral 1
 
     test "spiral of side length 2" $
-      (spiral 2) `equal` (to2DList [ [1, 2]
-                                   , [4, 3]
-                                   ])
+      equal (to2DList [ [1, 2]
+                      , [4, 3]
+                      ]) $ spiral 2
 
     test "spiral of side length 3" $
-      (spiral 3) `equal` (to2DList [ [1, 2, 3]
-                                   , [8, 9, 4]
-                                   , [7, 6, 5]
-                                   ])
+      equal (to2DList [ [1, 2, 3]
+                      , [8, 9, 4]
+                      , [7, 6, 5]
+                      ]) $ spiral 3
 
     test "spiral of side length 4" $
-      (spiral 4) `equal` (to2DList [ [ 1,  2,  3, 4]
-                                   , [12, 13, 14, 5]
-                                   , [11, 16, 15, 6]
-                                   , [10,  9,  8, 7]
-                                   ])
+      equal (to2DList [ [ 1,  2,  3, 4]
+                      , [12, 13, 14, 5]
+                      , [11, 16, 15, 6]
+                      , [10,  9,  8, 7]
+                      ]) $ spiral 4
 
     test "spiral of side length 5" $
-      (spiral 5) `equal` (to2DList [ [ 1,  2,  3,  4, 5]
-                                   , [16, 17, 18, 19, 6]
-                                   , [15, 24, 25, 20, 7]
-                                   , [14, 23, 22, 21, 8]
-                                   , [13, 12, 11, 10, 9]
-                                   ])
+      equal (to2DList [ [ 1,  2,  3,  4, 5]
+                      , [16, 17, 18, 19, 6]
+                      , [15, 24, 25, 20, 7]
+                      , [14, 23, 22, 21, 8]
+                      , [13, 12, 11, 10, 9]
+                      ]) $ spiral 5
 
 to2DList :: forall a. Array (Array a) -> List (List a)
 to2DList = fromFoldable <<< map fromFoldable
